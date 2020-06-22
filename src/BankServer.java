8c853e6568a323b53abaf90081f4b1ee2415768f
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-22 12:19:23 +0430

    Added Thread to BankServer

--- src/BankServer.java
+++ src/BankServer.java
@@ -9,9 +9,6 @@ public class BankServer {
     private int connectedClients;
     private int port;
     private Socket socket;
-    private ServerSocket serverSocket;
-    private DataInputStream dataInputStream;
-    private DataOutputStream dataOutputStream;
 
     public BankServer(String bankName, int dnsPort) {
         callDNS(bankName, dnsPort);
@@ -22,8 +19,8 @@ public class BankServer {
             accounts = new HashMap<>();
             connectedClients = 0;
             socket = new Socket("127.0.0.1", dnsPort);
-            dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-            dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             dataOutputStream.writeUTF("bank," + bankName);
             dataOutputStream.flush();
             port = dataInputStream.readInt();
@@ -34,19 +31,19 @@ public class BankServer {
 
     private void waitForClients() {
         try {
-            serverSocket = new ServerSocket(port);
+            ServerSocket serverSocket = new ServerSocket(port);
             while(true) {
                 socket = serverSocket.accept();
-                executeTransaction(socket);
-                socket.close();
+                connectedClients++;
+                new ClientHandler(socket, this);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void executeTransaction(Socket socket) throws IOException {
-        dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+    private synchronized void executeTransaction(DataInputStream dataInputStream, DataOutputStream dataOutputStream)
+            throws IOException {
         String transaction = dataInputStream.readUTF();
         int userId = Integer.parseInt(transaction.split(",")[0]);
         int amount = Integer.parseInt(transaction.split(",")[1]);
@@ -64,7 +61,6 @@ public class BankServer {
                 }
             }
         }
-        dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
         dataOutputStream.writeUTF("Successful!");
         dataOutputStream.flush();
     }
@@ -75,5 +71,30 @@ public class BankServer {
 
     public int getNumberOfConnectedClients() {
         return connectedClients;
+    }
+
+    static class ClientHandler extends Thread {
+        BankServer bankServer;
+        Socket socket;
+        DataOutputStream dataOutputStream;
+        DataInputStream dataInputStream;
+
+        public ClientHandler(Socket socket, BankServer bankServer) throws IOException {
+            this.bankServer = bankServer;
+            this.socket = socket;
+            dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        }
+
+        @Override
+        public void run() {
+            try {
+                bankServer.executeTransaction(dataInputStream, dataOutputStream);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bankServer.connectedClients--;
+        }
     }
 }
